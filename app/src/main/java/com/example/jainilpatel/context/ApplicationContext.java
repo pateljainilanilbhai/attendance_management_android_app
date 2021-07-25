c49cac8223c62041586bff947bccc979403d1080
jainil <jainil@infocusp.in>
2021-07-26 03:58:50 +0530

    first init

---
+++ app/src/main/java/com/example/jainilpatel/context/ApplicationContext.java
@@ -0,0 +1,47 @@
+package com.example.jainilpatel.context;
+
+import java.util.ArrayList;
+
+import android.app.Application;
+
+import com.example.jainilpatel.bean.AttendanceBean;
+import com.example.jainilpatel.bean.AttendanceSessionBean;
+import com.example.jainilpatel.bean.FacultyBean;
+import com.example.jainilpatel.bean.StudentBean;
+import com.example.jainilpatel.context.ApplicationContext;
+import com.example.jainilpatel.db.DBAdapter;
+
+public class ApplicationContext extends Application {
+	private FacultyBean facultyBean;
+	private AttendanceSessionBean attendanceSessionBean;
+	private ArrayList<StudentBean> studentBeanList;
+	private ArrayList<AttendanceBean> attendanceBeanList;
+	
+	public FacultyBean getFacultyBean() {
+		return facultyBean;
+	}
+	public void setFacultyBean(FacultyBean facultyBean) {
+		this.facultyBean = facultyBean;
+	}
+	public AttendanceSessionBean getAttendanceSessionBean() {
+		return attendanceSessionBean;
+	}
+	public void setAttendanceSessionBean(AttendanceSessionBean attendanceSessionBean) {
+		this.attendanceSessionBean = attendanceSessionBean;
+	}
+	public ArrayList<StudentBean> getStudentBeanList() {
+		return studentBeanList;
+	}
+	public void setStudentBeanList(ArrayList<StudentBean> studentBeanList) {
+		this.studentBeanList = studentBeanList;
+	}
+	public ArrayList<AttendanceBean> getAttendanceBeanList() {
+		return attendanceBeanList;
+	}
+	public void setAttendanceBeanList(ArrayList<AttendanceBean> attendanceBeanList) {
+		this.attendanceBeanList = attendanceBeanList;
+	}
+	
+	
+
+}
